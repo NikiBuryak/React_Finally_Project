ccd246a709e27a34fd3c8fd9b4114109d6a2427a
Nikita <nikiburyak@gmail.com>
2021-07-08 15:44:17 +0300

    Project 1 step

--- project/src/features/feature/pages/CatalogPage.js
+++ project/src/features/feature/pages/CatalogPage.js
@@ -5,48 +5,108 @@ import Box from "@material-ui/core/Box";
 import {Button, Container, makeStyles, Typography} from "@material-ui/core";
 import {Link} from "react-router-dom";
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
 const useStyles = makeStyles((theme) => ({
     postContainer: {
+        margin: 0,
         marginBottom: theme.spacing(5),
+        width: '30%',
+
     },
+    root: {
+        maxWidth: 345,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100%'
+    },
+    media: {
+        height: 140,
+    },
+    postsContainer: {
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "row"
+    }
+
 }));
+const theme = {
+    spacing: 10,
+}
 
 export function CatalogPage() {
 
-    const customStyles = useStyles();
+    const classes = useStyles();
+
 
     const {data, error, isLoading} = useQuery('catalog', async () => {
         const {data} = await getCatalog()
         return data;
     });
+
+
     return (
         <div className="page">
             {isLoading ? <div>Lading...</div>
                 : error ? <div>Error : {error}</div>
                     : <div>
-                        {data.map((post) => (
-                            <Container className={customStyles.postContainer} key={post.id}>
-                                <h3>{post.title}</h3>
-                                <p>{post.description}</p>
-                                <div>Categories:
-                                    {post.categories.map((category) => <span key={category}>{category},</span>)}
-                                </div>
-                                <Box display="flex">
-                                    {post.isInStock &&
-                                    <Typography variant="h6" mr={2} color="error">IN STOCK!!!</Typography>}
-                                    {post.isNew && <Typography variant="h6" color="error">NEW!!!</Typography>}
-                                    {post.isSale && <Typography variant="h6" color="error">ON SALE!!!</Typography>}
-                                </Box>
-                                <p>Price : {post.price}</p>
-                                <img src={post.photo}/>
-                                <Typography variant="h6" color='inherit'>Rating : {post.rating}</Typography>
-                                <Button variant="outlined" to={`/catalog/${post.id}`} exact component={Link}>Detailed {post.id}</Button>
-                            </Container>
-                        ))}
+                        <Box mb={5}>
+                            <Typography>Filters</Typography>
+                            <Box>Price</Box>
+                            <Box>Rating</Box>
+                            <Box>New</Box>
+                            <Box>Sale</Box>
+                            <Box>In Stock</Box>
+                        </Box>
+                        <Container className={classes.postsContainer}>
+                            {data.map((post) => (
+                                <Container className={classes.postContainer} key={post.id}>
+                                    <Card className={classes.root}>
+                                        <CardContent>
+                                            <h3>{post.title}</h3>
+                                            <p>{post.description}</p>
+                                            <ul>Categories:
+                                                {post.categories.map((category) => category && <li
+                                                    key={category}>{category}</li>)}
+                                            </ul>
+                                            <Box>
+                                                {post.isInStock &&
+                                                <Typography variant="h6" color="error">IN STOCK!!!</Typography>}
+                                                {post.isNew &&
+                                                <Typography variant="h6" color="error">NEW!!!</Typography>}
+                                                {post.isSale &&
+                                                <Typography variant="h6" color="error">ON SALE!!!</Typography>}
+                                            </Box>
+                                            <p>Price : {post.price}</p>
+                                        </CardContent>
+                                        <CardMedia
+                                            className={classes.media}
+                                            image={post.photo}
+                                        />
+                                        <CardContent>
+                                            <Typography variant="h6" color='inherit'>Rating
+                                                : {post.rating}</Typography>
+                                        </CardContent>
+
+                                        <CardActions>
+                                            <Button size="small" color="primary" variant="outlined"
+                                                    to={`/catalog/${post.id}`} exact
+                                                    component={Link}>
+                                                Detailed
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Container>
+                            ))}
+                        </Container>
                     </div>
             }
         </div>
     );
 }
 
-CatalogPage.propTypes = {};
+;
--- project/src/features/feature/pages/ProductPage.js
+++ project/src/features/feature/pages/ProductPage.js
@@ -1,31 +1,91 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Box from "@material-ui/core/Box";
 import {useParams} from "react-router";
-import {Button, Container} from "@material-ui/core";
-import {Link} from "react-router-dom";
+import {Button, Container, makeStyles, Typography} from "@material-ui/core";
+import {Link, NavLink} from "react-router-dom";
 import {useQuery} from "react-query";
-import {get} from "../api/ProductsAPI";
+import {get, getCatalog} from "../api/ProductsAPI";
+import Badge from '@material-ui/core/Badge';
+
+const useStyles = makeStyles((theme) => ({
+    productText: {
+        marginBottom: theme.spacing(2)
+    }
+}))
+const theme = {
+    spacing: 10,
+}
+
 
 export function ProductPage() {
     const {id} = useParams();
-    console.log(id)
+    const classes = useStyles();
 
     const {data, error, isLoading} = useQuery('product', async () => {
         const {data} = await get(id)
         return data;
     });
-    console.log(data)
+
+    const list = useQuery('productsList', async () => {
+        const {data} = await getCatalog()
+        return data;
+    });
+
+    const findList = (arr) => {
+        let newArr = arr.filter((el) => el.categories.includes(data.categories[0]) || el.categories.includes(data.categories[1]))
+        return newArr;
+    }
 
     return (
         <Container>
             <Box>
                 {isLoading ? <div>Loading...</div>
                     : error ? <div>Error: {error}</div>
-                        : <div>{data.title}</div>
-                }
+                        : <Container>
+                            <Typography className={classes.productText} variant="h3">{data.title}</Typography>
+                            <img src={data.photo}/>
+                            <Box display="flex">
+                                {data.isInStock &&
+                                <Badge color="error" variant="dot">
+                                    <Typography variant="h6" mr={2} color="inherit">IN STOCK!!!</Typography>
+                                </Badge>}
+                                {data.isNew &&
+                                <Badge color="error" variant="dot">
+                                    <Typography variant="h6" color="inherit">NEW!!!</Typography>
+                                </Badge>}
+                                {data.isSale &&
+                                <Badge color="error" variant="dot">
+                                    <Typography variant="h6" color="inherit">ON SALE!!!</Typography>
+                                </Badge>}
+                            </Box>
+                            <p>Price : {data.price}</p>
+                            <Typography className={classes.productText} variant="h6" color='inherit'>Rating
+                                : {data.rating}</Typography>
+
+                            <Button variant="outlined" disabled={!data.isInStock}>
+                                {data.isInStock ? 'Buy' : 'Out of stock'}
+                            </Button>
+                            <Button variant="outlined" to='/catalog/' exact component={Link}>Back</Button>
+                            <ul className={classes.productText}>Categories:
+                                {data.categories.map((category) => <li key={category}>{category}</li>)}
+                            </ul>
+                            <Typography>{data.description}</Typography>
+                            <Box mt={5}>
+                                <Typography variant="h5">Similar products</Typography>
+                                {list.isLoading ? <div>Loading some products...</div>
+                                    : list.error ? <div>Error loading : {list.error}</div>
+                                        : findList(list.data).slice(2, 6).map((el) => (
+                                            <div key={el.id}>
+                                                {el.title}
+                                                <p>{el.price}</p>
+                                            </div>
+                                        ))}
+                            </Box>
+                        </Container>}
+
             </Box>
-            <Button variant="outlined" to='/catalog/' exact component={Link}>Posts</Button>
         </Container>
+
 
     )
 }
